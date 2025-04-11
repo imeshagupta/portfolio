ecf8e1658886eb7a0df30afe13fbf1edf61e0a82
esha-27 <eshagupta515@gmail.com>
2025-04-11 18:06:17 +0530

    Added one project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,34 +3,43 @@ import multiFusionImg from "../assets/project1.png";
 import ninjaQuizImg from "../assets/project2.png";
 import portfolioImg from "../assets/project3.png";
 import fund4EduImg from "../assets/project4.png";
+import sukoonNamaImg from "../assets/project5.png";
 
 const projectList = [
   {
     title: "Multi-Fusion Convert",
     description:
-      "A versatile unit conversion tool supporting various categories.",
+      "A dynamic unit conversion tool that supports multiple categories like temperature, weight, distance, etc. Built with React, offering a smooth and responsive experience for everyday practical use.",
     image: multiFusionImg,
     link: "https://multi-fusion-convert.netlify.app/",
   },
   {
     title: "Ninja Coding Quiz",
     description:
-      "An interactive quiz app featuring mcqs for coding enthusiasts who wants some fun.",
+      "A stylish quiz app designed for developers to test coding knowledge through MCQs. Covers topics like HTML, CSS, and JavaScript in a fun and interactive format.",
     image: ninjaQuizImg,
     link: "https://ninja-coding-quiz.netlify.app/",
   },
   {
     title: "Portfolio Website",
-    description: "A sleek personal portfolio built with React & Bootstrap.",
+    description:
+      "A responsive personal portfolio to showcase projects, skills, and contact information. Designed using React and Bootstrap with smooth animations and a modern, clean layout.",
     image: portfolioImg,
     link: "#",
   },
   {
     title: "Fund4Edu",
     description:
-      "A funding platform to help students receive financial support.",
+      "A conceptual web platform aimed at helping students find funding opportunities for education. Features an intuitive layout, easy navigation, and a focus on accessibility and impact.",
     image: fund4EduImg,
     link: "#",
+  },
+  {
+    title: "SukoonNama",
+    description:
+      "A poetic platform that presents Hindi shayaris and poems categorized by themes. Built using React and Firebase, it offers a flowery, elegant UI that matches its soulful content.",
+    image: sukoonNamaImg,
+    link: "https://sukoonnama.netlify.app/",
   },
 ];
 
